749ce90080fbadce5566234ad6fe081656b8c777
Tony <un_razum@mail.ru>
2021-08-22 18:41:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const messageInput = document.querySelector("#message-input");
 const subBtn = document.querySelector("#submit");
 const messageP = document.querySelector("#mesage-output");
 subBtn.addEventListener("click", getMessage);
+messageInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    getMessage();
+  }
+});
 
-
-function getMessage() {}
-  
+function getMessage() {
   messageP.textContent = messageInput.value;
   messageInput.value = "";
 }
